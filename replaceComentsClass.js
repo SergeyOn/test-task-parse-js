22ed1d68e24b29cbb4fd529406822122bd7db721
SergeyOn <seregapantera@gmail.com>
2019-11-06 03:30:53 +0200

    class variation

---
+++ replaceComentsClass.js
@@ -0,0 +1,54 @@
+/* usage 
+
+class variation
+
+https://github.com/SergeyOn/
+
+;url = [string]
+;const replace = new ReplaceComments();
+;let result = await replace.fetchUrl('url');
+;replace.showData() || result;
+
+*/
+
+class ReplaceComments {
+  constructor() {
+    this.text = '';
+    this.parsed = '';
+    this.resultFunc = () => alert('hello world');
+    this.regExp = /\/\*(.|\n)*?\*\/|(?<![\\,",',`])\/{2}.*?\n/gms;
+  };
+
+  showData = () => ({
+    before: this.text,
+    after: this.parsed,
+    function: this.resultFunc,
+  });
+
+  parseText = () => this.text.replace(this.regExp, '');
+
+  refresh = (text) => {
+    this.text = text;
+    this.parsed = this.parseText();
+    this.resultFunc = this.parsed ? new Function(this.parsed) : () => alert('hello world');
+  };
+
+  fetchUrl = async url => {
+
+    try {
+      const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(response.statusloadText);
+      }
+      const responseText = await response.text();
+      this.refresh(responseText);
+    } catch (err) {
+      this.refresh('');
+      throw new Error(err);
+    }
+
+    return showData();
+  };
+};
+
+
